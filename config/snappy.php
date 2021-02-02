2a8fe1042d28ee1bffad789379ce33666f3c3254
vcromeiro <victor@apus.info>
2021-02-02 15:57:45 +0000

    chore: apus deploy

--- config/snappy.php
+++ config/snappy.php
@@ -5,7 +5,7 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => env('WKHTMLTOPDF_PATH', 'C:/wkhtmltopdf/bin/wkhtmltopdf.exe'),
+        'binary'  => env('WKHTMLTOPDF_PATH', 'wkhtmltopdf'),
         'timeout' => false,
         'options' => [],
         'env'     => [],
